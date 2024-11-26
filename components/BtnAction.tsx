bc859cec9cdbd0f6ee716611334b2c447443db39
gabrielfelipedy <gabriel.felipe.dy@gmail.com>
2024-11-26 11:54:55 -0300

    finalizar componente hero

---
+++ components/BtnAction.tsx
@@ -0,0 +1,20 @@
+interface BtnProps {
+  caption: string;
+  styles: string;
+}
+
+const BtnAction = ({ caption, styles }: BtnProps) => {
+  return (
+    <div className="flex justify-center items-center">
+
+        <div className={`${styles} border border-text-primary px-8 py-4 rounded-2xl`}>
+          <p className="text-center text-2xl font-garet text-primary">
+            {caption}
+          </p>
+        </div>
+      
+    </div>
+  );
+};
+
+export default BtnAction;
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import BtnAction from '@/components/BtnAction'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className="w-full bg-background-primary text-text-primary">
+
+        <div className="relative w-full p-8 max-container bg-hero bg-cover bg-no-repeat bg-center">
+
+          <div className="w-full md:w-4/6">
+            <p className='text-hero-text mt-14 md:mt-40 mb-16'>
+              Dra.
+            </p>
+
+            <h1 className="text-hero-mobile md:text-hero leading-none font-garet text-dark-brown">Camila</h1>
+            <h1 className="text-hero-mobile md:text-hero leading-none font-garet text-dark-brown">Neiva</h1>
+
+            <div className="bg-primary py-4">
+              <p className="text-subtitle-mobile md:text-subtitle leading-none font-garet">
+                Psiquiatra em Macapá - AP
+              </p>
+            </div>
+          </div>
+
+          <div className='flex flex-col md:flex-row items-start md:items-end justify-between'>
+            <p className='text-hero-text w-full md:w-5/12 mx-auto md:mx-0'>
+              Não se trata de uma consulta, é o primeiro passo da sua jornada de transformação.
+            </p>
+            <BtnAction styles='mt-5' caption='Agende sua consulta' />
+          </div>
+
+        </div>
+    </section>
   )
 }
 
